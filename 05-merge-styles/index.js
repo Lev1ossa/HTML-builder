24b882bb538fa97f7dbbf5d038b81f2052c39a1a
Lev1ossa <ikehhui@gmail.com>
2023-05-08 19:23:22 +0600

    feat: implement solution subtask 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,30 @@
+const { join, extname } = require('path');
+const { readdir, stat } = require('fs/promises');
+const {createReadStream, createWriteStream} = require('fs');
+
+const stylesFolderPath = join(__dirname, 'styles');
+const bundlePath = join(__dirname, 'project-dist', 'bundle.css');
+
+const writeStream = createWriteStream(bundlePath);
+
+const createBundle = async () => {
+  try {
+    const files = await readdir(stylesFolderPath);
+    files.forEach(async (file) => {
+      const styleFilePath = join(stylesFolderPath, file);
+      try {
+        const stats = await stat(styleFilePath);
+        if(stats.isFile() && extname(styleFilePath) === '.css'){
+          const readStream = createReadStream(styleFilePath);
+          readStream.pipe(writeStream);
+        }
+      } catch (err) {
+        console.error(err.message);
+      }
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+createBundle();
