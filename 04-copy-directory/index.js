6c4db5822f44678349c6fddbbe069bbaa04517c5
Lev1ossa <ikehhui@gmail.com>
2023-05-08 18:28:12 +0600

    feat: implement solution subtask 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,53 @@
+const { join } = require('path');
+const { readdir, mkdir, rm, access, copyFile } = require('fs/promises');
 
+const folderPath = join(__dirname, 'files');
+const folderCopyPath = join(__dirname, 'files-copy');
+
+const copyDir = async () => {
+
+  const removeDir = async () => {
+    try {
+      await rm(folderCopyPath, { recursive: true });
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  const createDir = async () => {
+    try {
+      await mkdir(folderCopyPath, { recursive: true });
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  //check folder exists
+  try {
+    //exists, delete dir, create new dir
+    await access(folderCopyPath);
+    await removeDir();
+    await createDir();
+  } catch (err) {
+    //dont exist, create new dir
+    createDir();
+  }
+
+  //copy dir
+  try {
+    const files = await readdir(folderPath);
+    files.forEach(async (file) => {
+      const filePath = join(folderPath, file);
+      const fileCopyPath = join(folderCopyPath, file);
+      try {
+        await copyFile(filePath, fileCopyPath);
+      } catch (err) {
+        console.error(err.message);
+      }
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+copyDir();
